82ed1163abed2f96da6154d98c036723564fdbb4
ilhan007 <ilhan.orhan007@gmail.com>
2016-02-13 23:29:32 +0200

    Added package.json

--- public/app/list/Item.service.js
+++ public/app/list/Item.service.js
@@ -4,19 +4,19 @@
     angular.module('bidit')
         .factory('ItemService', function() {
             return [{
-            	id: "#43434234",
+            	id: "#13434234",
                 title: "item  1",
                 description: "Lenovo 8GB i7",
                 price: 260,
                 remaining: "32h:5m:4s"
             }, {
-            	id: "#43434234",
+            	id: "#23434234",
                 title: "item  2",
                 description: "Lenovo 4GB i5",
                 price: 300,
                 remaining: "32h:5m:4s"
             }, {
-            	id: "#43434234",
+            	id: "#33434234",
                 title: "item  3",
                 description: "HP 16GB i7",
                 price: 80,
