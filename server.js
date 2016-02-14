f0e1e46f5b4927b02d5235badeb3b1dd53233000
ilhan007 <ilhan.orhan007@gmail.com>
2016-02-14 11:41:24 +0200

    Enabled node server

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
-//Lets require/import the HTTP module
-var http = require('http');
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
 
 //Lets define a port we want to listen to
-const PORT = 8080;
+var PORT = 8080;
+
+//Lets define the dir we serve from
+var dirname = "/public";
 
 //We need a function which handles requests and send response
 function handleRequest(request, response) {
-	response.end('Hello World! Path Hit: ' + request.url);
-}	
+    response.sendFile('index.html', { root: __dirname + dirname});
+}
 
-//Create a server
-var server = http.createServer(handleRequest);
+app.get('/', handleRequest);
+app.use(express.static(__dirname));
 
 //Lets start our server
-server.listen(PORT, function() {
-	//Callback triggered when server is successfully listening. Hurray!
-	console.log("Server listening on: http://localhost:%s", PORT);
+http.listen(PORT, function() {
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
 });
